236646f609d2ef08f5fe2e73250bd55770ea36b0
saipraneeth <2506664+msaipraneeth@users.noreply.github.com>
2023-10-09 07:20:13 +0100

    update ulid transform plugin

--- cmem_plugin_ulid/transform/__init__.py
+++ cmem_plugin_ulid/transform/__init__.py
@@ -1,74 +1,35 @@
 """lifetime(age) transform plugin module"""
-import datetime
-from datetime import date
 from typing import Sequence
 
 from cmem_plugin_base.dataintegration.description import (
     Plugin,
-    PluginParameter,
 )
 from cmem_plugin_base.dataintegration.plugins import TransformPlugin
+from ulid import ULID
 
 
 @Plugin(
-    label="Lifetime (age - ulid)",
-    plugin_id="Example-Lifetime-ulid",
-    description="From the input date,"
-    "the value gets transformed into number of years (age)."
-    " Supports only xsd:date(YYYY-MM-DD) format.",
+    label="ULID",
+    plugin_id="cmem-plugin-ulid",
+    description="Generate a ULID from a random number, and the current time.",
     documentation="""
-This example transform operator returns lifetime(age).
+This ulid transform operator generates random lexicographically sortable ulid.
 
-From the input date,
-the value gets transformed into number of years(age).
+Generates random ULID, based on length of inputs, if their are no inputs.
+then it will generate one ULID.
 
-Input Date:         2000-05-22
-Current Date:       2022-08-19
-Transformed Output: 22
-
-The parameter can be specified:
-
-- 'date': specify a date in xsd:date(yyyy-MM-dd) format
 """,
     parameters=[
-        PluginParameter(
-            name="start_date",
-            label="Date",
-            description="specify a date to know its lifetime(age).",
-            default_value=None,
-        ),
     ],
 )
-class Lifetime(TransformPlugin):
-    """Lifetime Transform Plugin"""
-
-    DATE_FORMAT = "%Y-%m-%d"
-
-    def __init__(self, start_date: str):
-        self.start_date = start_date
+class ULIDTransformPlugin(TransformPlugin):
+    """ULID Transform Plugin"""
 
     def transform(self, inputs: Sequence[Sequence[str]]) -> Sequence[str]:
         result = []
         if len(inputs) != 0:
             for collection in inputs:
-                result += [f"{self._calculate_age(_)}" for _ in collection]
-        if len(result) == 0 and len(self.start_date) > 0:
-            result += [f"{self._calculate_age(self.start_date)}"]
+                result += [f"{ULID()}" for _ in collection]
+        if len(result) == 0:
+            result += [f"{ULID()}"]
         return result
-
-    def _calculate_age(self, value: str) -> int:
-        """calculate age in years"""
-        today = date.today()
-        born = datetime.datetime.strptime(value, self.DATE_FORMAT).date()
-        try:
-            birthday = born.replace(year=today.year)
-
-        # raised when birthdate is February 29
-        # and the current year is not a leap year
-        except ValueError:
-            birthday = born.replace(year=today.year, month=born.month + 1, day=1)
-
-        if birthday > today:
-            return today.year - born.year - 1
-
-        return today.year - born.year
